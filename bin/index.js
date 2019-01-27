2a3ddcebf607c43e67e3dc4654fa2777c41cd275
Zack Pace <zack@fbts.ca>
2019-01-27 16:25:36 -0600

    Test module

--- bin/index.js
+++ bin/index.js
@@ -15,31 +15,16 @@ Run the script to host your package manager Walter.
 */
 
 (function() {
-  var filesPath = wconfig.filesPath;
+  var express    = require('express')
+  var serveIndex = require('serve-index')
 
-  var app = express();
-  app.all('*', function(req, res) {
-    var args = req.url.split("/");
-    if(!args[0] == "job") {
-      if(!typeof wconfig.reroute == "undefined") {
-        require(path.join("../"), wconfig.callback)(req, res);
-      } else {
-        werror("error", "ReRoute not called, showing default page.")
-      }
-    } else {
-      var page = "<h1>Walter</h1>";
-      if(!typeof args[1] == "undefined") {
-        res.send(page);
-        if(fs.existsSync(path.resolve(path.join("../", filesPath, args.slice(1))))) {
-          res.send(fs.readFileSync(path.resolve(path.join("../", filesPath, args.slice(1)))));
-        } else {
-          fs.readDirSync(path.resolve(path.join("../", filesPath))).forEach(function(fileName) {
-            res.send("<a href=\"" + "job/" + path.join("../", filesPath) + "\"" + fileName + "</a>")
-          })
-        }
-      }
-    }
-  })
+  var app = express()
 
-  app.listen(8080)
+  // Serve URLs like /ftp/thing as public/ftp/thing
+  // The express.static serves the file contents
+  // The serveIndex is this module serving the directory
+  app.use('/job', express.static('files'), serveIndex('public/ftp', {'icons': true}))
+
+  // Listen
+  app.listen(3000)
 })()
