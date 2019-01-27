605d946c8f3caabb050bfbf1711eee9e968f0152
Zack Pace <zack@fbts.ca>
2019-01-27 15:14:47 -0600

    Update and add tester

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
-var wconfig = require("walt-config.js"),
+var wconfig = require("./walt-config.js"),
     glob = require("glob"),
     fs = require("fs"),
     path = require("path"),
     express = require("express"),
-    werror = require("walt-error.js");
+    werror = require("./walt-error.js");
 
 /*
 有关此脚本的完整文档，请参阅GitHub wiki。
@@ -27,7 +27,19 @@ Run the script to host your package manager Walter.
         werror("error", "ReRoute not called, showing default page.")
       }
     } else {
-      
+      var page = "<h1>Walter</h1>";
+      if(!typeof args[1] == "undefined") {
+        res.send(page);
+        if(fs.existsSync(path.resolve(path.join("../", filesPath, args.slice(1))))) {
+          res.send(fs.readFileSync(path.resolve(path.join("../", filesPath, args.slice(1)))));
+        } else {
+          fs.readDirSync(path.resolve(path.join("../", filesPath))).forEach(function(fileName) {
+            res.send("<a href=\"" + "job/" + path.join("../", filesPath) + "\"" + fileName + "</a>")
+          })
+        }
+      }
     }
   })
+
+  app.listen(8080)
 })()
