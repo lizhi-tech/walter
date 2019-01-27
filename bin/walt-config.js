4f89f5197b018c0c2d7866730b0ad2f400b7cc75
Zack Pace <zack@fbts.ca>
2019-01-26 19:54:56 -0600

    Update walt-config module

--- bin/walt-config.js
+++ bin/walt-config.js
@@ -1,3 +1,7 @@
 module.exports = function() {
+  var fs = require("fs"),
+      path = require("path");
 
+  var fileContent = fs.readFileSync(path.resolve("../walter.json"));
+  return JSON.parse(fileContent);
 }
