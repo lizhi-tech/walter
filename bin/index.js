f0f9c0d583a6b0669c0543a855dbee08b50f0fdb
Zack Pace <zack@fbts.ca>
2019-01-26 19:59:48 -0600

    Update index, documentate :)

--- bin/index.js
+++ bin/index.js
@@ -3,3 +3,16 @@ var wconfig = require("walt-config.js"),
     fs = require("fs"),
     path = require("path"),
     express = require("express");
+
+/*
+有关此脚本的完整文档，请参阅GitHub wiki。
+运行脚本来托管您的包管理器Walter。
+
+
+For complete documentation on this script, see the GitHub wiki.
+Run the script to host your package manager Walter.
+*/
+
+(function() {
+  
+})
